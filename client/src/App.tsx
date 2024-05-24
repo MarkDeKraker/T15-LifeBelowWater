879727100e4656de80da4fb3df59979f6113c60f
Faroeq <alves_1044@hotmail.com>
2024-05-24 17:26:34 +0200

    Add 404 fallback page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ import HomePage from "./components/HomePage";
 import { useState } from "react";
 import Layout from "./components/Layout";
 import QuizOverviewPage from "./components/QuizOverviewPage";
+import QuizPage from "./pages/QuizPage";
 import QuizLoginPage from "./pages/QuizLoginPage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   const [active, setActive] = useState<number>(0);
@@ -16,7 +18,11 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/quiz/overview" element={<QuizOverviewPage />} />
+            {/* quiz/login/id is de url die de leerling krijgt om in te loggen */}
             <Route path="/quiz/login/:id" element={<QuizLoginPage />} />
+            {/* quiz/:slug is de url waar de leerling de quiz start*/}
+            <Route path="/quiz/:slug" element={<QuizPage />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </BrowserRouter>
