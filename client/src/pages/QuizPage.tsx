ae721d01f84757ef44bf904c1bb7f63214152845
Faroeq <alves_1044@hotmail.com>
2024-05-25 14:53:40 +0200

    Add quizpage

---
+++ client/src/pages/QuizPage.tsx
@@ -0,0 +1,11 @@
+import Quiz from "../components/quiz/Quiz";
+
+/*
+Dit is de pagina wanneer de inlogcode correct is. De gebruiker wordt naar deze pagina doorgestuurd.
+*/
+
+function QuizPage() {
+  return <Quiz />;
+}
+
+export default QuizPage;
