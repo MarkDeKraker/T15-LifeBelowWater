9ed4acdff47cb9f079f0c1d151a5910ae36e70a4
Nelio-J <neliojarmohamed@gmail.com>
2024-05-29 20:59:06 +0200

    Added an up/down arrow next to the button.
    Added framer motion animation for the collapse section.

--- client/src/components/buttons/ExpandButton.tsx
+++ client/src/components/buttons/ExpandButton.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+import { motion } from "framer-motion";
+
+import {
+  ArrowDownIcon, ArrowUpIcon
+} from "@heroicons/react/24/solid";
+
 type Props = {
   children: React.ReactNode;
   expandedText: React.ReactNode;
@@ -18,11 +24,30 @@ export function ExpandButton(props: Props) {
     <>
       <button
         onClick={toggleCollapse}
-        className={`text-black hover:underline px-4 py-2 mt-5 rounded-full caption-top transition duration-200 ease-in-out focus:ring-opacity-50`}
+        className={`text-black hover:underline px-4 py-2 mt-5 rounded-full caption-top transition duration-200 ease-in-out focus:ring-opacity-50 inline-flex`}
       >
         {props.children}
+        <motion.span
+          animate={{ opacity: 1}}
+          transition={{ duration: 0.4, ease: "easeInOut"}}
+          className="ml-2 text-black"
+        >
+          {collapsed ? (
+            <ArrowUpIcon className="inline-flex w-4 h-4" />
+          ) : (
+            <ArrowDownIcon className="inline-flex w-4 h-4" />
+          )}
+        </motion.span>
       </button>
-      {collapsed && <div className="mt-3 text-[#666] max-w-xl font-custom">{expandedText}</div>}
+
+      <motion.div
+        initial={false}
+        animate={{ height: collapsed ? "auto" : 0, opacity: collapsed ? 1 : 0 }}
+        transition={{ duration: 0.7, ease: "easeInOut"}}
+        className="overflow-hidden"
+      >
+        <div className="mt-3 text-[#666] max-w-xl font-custom">{expandedText}</div>
+      </motion.div>
     </>
   );
 }
