020b2b5894a6b929c18216ceffd7f60f82bbdb54
Nelio-J <127200545+Nelio-J@users.noreply.github.com>
2024-05-31 14:07:15 +0200

    14 als docent wil ik de belangrijkste termen van elk onderwerp weten (#52)
    * Added an expandable section on the homepage, that displays important terms from a specific topic.
    
    Tweaked the content in the plastic pages.
    
    * Added overflow-hidden to the parent div and overflow-y-auto to the sidebar. This makes it so the background and sidebar don't get cut off when the page becomes scrollable.
    
    * Added an up/down arrow next to the button.
    
    Added framer motion animation for the collapse section.
    
    * Remove extra text component, Move homepage to page
    
    * fix unlinked prop.To
    
    ---------
    
    Co-authored-by: Faroeq <alves_1044@hotmail.com>

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -4,9 +4,9 @@ import SideBar from "./SideBar";
 
 const Layout = ({ children }: { children: any }) => {
   return (
-    <div className="flex h-full bg-amber-50">
+    <div className="flex h-full bg-amber-50 overflow-hidden">
       <SideBar />
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full overflow-y-auto">
         <Content>{children}</Content>
       </div>
     </div>
