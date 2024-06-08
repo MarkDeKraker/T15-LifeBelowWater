9dd4e9b2d873e1ecfb963323097dff664b6c8dbd
Nelio-J <neliojarmohamed@gmail.com>
2024-06-08 13:34:08 +0200

    Added some animation to the title of the information pages.

--- client/src/components/Plastic/BannerComp.tsx
+++ client/src/components/Plastic/BannerComp.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const BannerComp = ({ source, alt, children }: { source: string, alt: string, children?: React.ReactNode }) => {
   return (
     <div className="relative w-full h-full z-20">
@@ -6,7 +8,13 @@ const BannerComp = ({ source, alt, children }: { source: string, alt: string, ch
         src={source}
         alt={alt}
       />
-      {children && <div className="absolute inset-0 flex items-center justify-center text-center text-white lg:text-8xl sm:text-7xl drop-shadow-2xl font-semibold font-custom mb-10 z-30">{children}</div>}
+      {children && <motion.div className="absolute inset-0 flex items-center justify-center text-center text-white lg:text-8xl sm:text-7xl drop-shadow-2xl font-semibold font-custom mb-10 z-30"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 1, duration: 1}}
+      >
+        {children}
+      </motion.div>}
     </div>
   );
 };
